b1c40e8c34a84f0322d4be574684806146d90b7a
Joshua Pc. Hdz <josh@Pineapple>
2022-07-10 15:11:27 -0500

    Implemented gennick in scheme lisp.

---
+++ scheme/scheme.scm
@@ -0,0 +1,26 @@
+#!/bin/guile -s
+!#
+
+;; Define le funny.
+(define vowels "aeiouy")
+(define consonant "bcdfghjklmnpqrstvwxz")
+;; Use the current time as a seed for random state.
+(define random-state (seed->random-state (car (gettimeofday))))
+
+;; Define a function to get a letter to get a more "functional" feel.
+(define (get-letter)
+  (if (= (random 2 random-state) 0)
+      (array-ref vowels (random 5))
+      (array-ref consonant (random 19))))
+
+(define (gen-nick)
+  (let ((limit (+ (random 6) 3)))
+    (do ((count 0 (1+ count))
+         (out "" (string-append out (string (get-letter)))))
+        ((= count limit) out))))
+
+;; Get n amount of nicks
+(define (get-nnicks n)
+  (do ((count 0 (1+ count))
+       (output '() (append (list (gen-nick)) output)))
+      ((= count n) output)))
