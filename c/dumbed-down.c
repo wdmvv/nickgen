3648550004d0c58e1c995e87f9021d34d3f905d9
code-explorer786 <68312688+code-explorer786@users.noreply.github.com>
2023-02-22 18:46:43 +0700

    use strtol and nanosecond resolution

--- c/dumbed-down.c
+++ c/dumbed-down.c
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <errno.h>
 
 #define DEFAULT_N_NAMES 1
 
@@ -26,7 +27,11 @@ const char consonants[] = "bcdfghjklmnpqrstvwxz";
 
 static inline void set_seed()
 {
-    srand(time(NULL));
+    // srand(time(NULL)); could work, but its resolution is just one second.
+    // This has nanosecond resolution, I guess.
+    struct timespec curr_time;
+    clock_gettime(CLOCK_REALTIME, &curr_time);
+    srand((curr_time.tv_sec << 3) | (curr_time.tv_nsec & 0x7));
 }
 
 void gen_nick()
@@ -50,7 +55,13 @@ int main(int argc, char* argv[]){
     set_seed();
     int n_names = DEFAULT_N_NAMES;
     // If arguments are supplied and is correct, set n_names.
-    // BUG: Strings like 123asdf still gets interpreted but should be invalid.
-    if(argc > 1) sscanf(argv[1], "%d", &n_names);
+    if(argc > 1){
+        char *after_num;
+        // For those who doesn't know, strtol returns the number from the beginning of a string and then sets the secondargument's variable (which is after_num in this case) to the beginning of the non-number string. 10 is the base.
+        n_names = strtol(argv[1], &after_num, 10);
+        // If there's an error or the string doesn't end past the number, reset the value.
+        if(errno || *after_num) n_names = DEFAULT_N_NAMES;
+        else n_names = n_names >= 0 ? n_names : 0;
+    }
     for(int i = 0; i < n_names; i++) gen_nick();
 }
